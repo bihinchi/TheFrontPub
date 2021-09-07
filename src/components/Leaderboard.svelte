e0267e46a15c291a57f6b133981836bb6b95da04
elbok <udratov@pm.me>
2021-09-07 17:00:55 +0200

    Leaderboard init

---
+++ src/components/Leaderboard.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { leaderboard } from "../js/stores"
+    leaderboard.scores = []
+</script>
+
+
+<div>
+    <h3>TOP records:</h3>
+
+    <ol>
+        { #each leaderboard.scores as topScore }
+            <li>
+                { topScore }
+            </li>
+        { /each }
+    </ol>
+
+</div>
--- src/js/stores.js
+++ src/js/stores.js
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 export const currentPub = writable({});
 export const history = writable([]);
+//export const leaderboard = writable({});
